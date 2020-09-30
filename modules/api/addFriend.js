a033fbf3b1d45b85559096c8e0ba4d772da4e6c9
GorbatovNik <gorbatov.nik@gmail.com>
2020-09-30 11:18:58 +0300

    addFriend не добавляет одного человека в друзья несколько раз

--- modules/api/addFriend.js
+++ modules/api/addFriend.js
@@ -6,17 +6,20 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 let ObjectID= require('mongodb').ObjectID;
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     let myId = req.body.myId;
     let friendId =req.body.friendId;
     const collection = req.app.locals.collectionUsers;
-    collection.updateOne({_id : ObjectID(myId)}, {$push : {friends : friendId}}, (err) => {
-        if(err) return console.log(err);
-    });
-    collection.updateOne({_id : ObjectID(friendId)}, {$push : {friends : myId}}, (err) => {
-        if(err) return console.log(err);
-        res.sendStatus(200);
-    });
+
+    let obj = await collection.findOne({_id : ObjectID(myId), friends : friendId})
+    if (obj==undefined) {
+      await collection.updateOne({_id : ObjectID(myId)}, {$push : {friends : friendId}})
+    }
+    obj = await collection.findOne({_id : ObjectID(friendId), friends : myId})
+    if (obj==undefined) {
+      await collection.updateOne({_id : ObjectID(friendId)}, {$push : {friends : myId}})
+    }
+    return res.sendStatus(200)
 })
 
 module.exports = router;
